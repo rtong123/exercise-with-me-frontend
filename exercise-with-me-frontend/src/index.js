11c512b36d7f3d9c509d7b1708144ad8d4b2172f
Rebecca Tong <rtong@buffalo.edu>
2020-05-23 18:41:20 -0400

    added store, reducer and composer

--- exercise-with-me-frontend/src/index.js
+++ exercise-with-me-frontend/src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+const enhancer = compose()
+
+//set up store
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

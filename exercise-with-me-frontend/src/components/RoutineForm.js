a05d64abc2687a60defab663a58cdd051c52eefa
Rebecca Tong <rtong@buffalo.edu>
2020-06-01 14:44:15 -0400

    changing things in form

--- exercise-with-me-frontend/src/components/RoutineForm.js
+++ exercise-with-me-frontend/src/components/RoutineForm.js
@@ -29,10 +29,10 @@ class RoutineForm extends React.Component{
         <form onSubmit={this.handleSubmit}>
           <label>Title:</label>
           <input type='text' value={this.state.title} name="title" onChange={this.handleChange}/><br></br>
-          <label>Body Weight:</label>
+          <label>Body Weight: (in lbs)</label>
           <input type='text' value={this.state.body_weight} name="body_weight" onChange={this.handleChange}/><br></br>
           <label>Start Date:</label>
-          <input type='text' value={this.state.start_date} name="start_date" onChange={this.handleChange}/><br></br>
+          <input type='text' placeholder='MM/DD/YYYY' value={this.state.start_date} name="start_date" onChange={this.handleChange}/><br></br>
           <label>Days:</label>
           <input type='text' value={this.state.days} name="days" onChange={this.handleChange}/><br></br>
           <input type='submit'></input>

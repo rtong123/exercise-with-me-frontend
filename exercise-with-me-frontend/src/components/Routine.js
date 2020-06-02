b52e24d4de3a44a9e1d07f17e5d1e6e4bc4f1c76
Rebecca Tong <rtong@buffalo.edu>
2020-06-01 21:49:25 -0400

    seeing id for routine

--- exercise-with-me-frontend/src/components/Routine.js
+++ exercise-with-me-frontend/src/components/Routine.js
@@ -6,17 +6,9 @@ const Routine = (props) => {
 
 let routine = props.routines[props.match.params.id - 1]
   return(
-    <div>
-      <b>Title: </b>{routine.title}
-  <br></br>
-  <b>Weight:</b> {routine.body_weight}
-  <br></br>
-  <b>Start Date:</b>{routine.start_date}
-  <br></br>
-  <b>Days:</b> {routine.days}
-  <br></br>
-  <br></br>
-      </div>
+<div>
+  {routine ? routine.title : null}
+</div>
   )
 }
 

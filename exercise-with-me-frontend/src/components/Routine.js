9019ae979006fd34e253cf05938491b4a05823cf
Rebecca Tong <rtong@buffalo.edu>
2020-06-03 15:01:48 -0400

    able to see one routine

--- exercise-with-me-frontend/src/components/Routine.js
+++ exercise-with-me-frontend/src/components/Routine.js
@@ -11,7 +11,10 @@ let routine = props.routines[props.match.params.id - 1]
 
   return(
     <div>
-        {routine ? routine.title : null}
+        <b>Routine Title:</b> {routine ? routine.title : null} <br></br>
+        <b>Body Weight:</b> {routine ? routine.body_weight : null} <br></br>
+        <b> Start Date: </b> {routine ? routine.date : null} <br></br>
+        <b> Days: </b> {routine ? routine.days : null}<br></br>
     </div>
     )
   }

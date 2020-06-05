18e036cfd7ed17999eb334cd99c267124b0f3555
Rebecca Tong <rtong@buffalo.edu>
2020-06-05 14:21:14 -0400

    added swtich

--- exercise-with-me-frontend/src/components/Routine.js
+++ exercise-with-me-frontend/src/components/Routine.js
@@ -11,6 +11,7 @@ let routine = props.routines[props.match.params.id - 1]
 
   return(
     <div>
+      <h1>Routine {props.match.params.id}</h1>
         <b>Routine Title:</b> {routine ? routine.title : null} <br></br>
          <b>Body Weight:</b> {routine ? routine.body_weight : null} <br></br>
          <b> Start Date: </b> {routine ? routine.date : null} <br></br>
--- exercise-with-me-frontend/src/containers/RoutinesContainer.js
+++ exercise-with-me-frontend/src/containers/RoutinesContainer.js
@@ -4,7 +4,7 @@ import Routine from '../components/Routine'
 import RoutineForm from '../components/RoutineForm'
 import {connect} from 'react-redux'
 import {fetchRoutines} from '../actions/fetchRoutines'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 
 class RoutinesContainer extends React.Component{
   componentDidMount(){
@@ -14,11 +14,12 @@ class RoutinesContainer extends React.Component{
   render(){
     return(
       <div>
+        <Switch>
       <Route path='/routines/new' component={RoutineForm}/>
       <Route path='/routines/:id' render={(routerProps) => <Routine {...routerProps} routines={this.props.routines}/>}/>
-      <Route exact path='/routines' render={(routerProps) => <Routines {...routerProps} routines={this.props.routines}/>}/>
-
+      <Route path='/routines' render={(routerProps) => <Routines {...routerProps} routines={this.props.routines}/>}/>
       <br></br>
+      </Switch>
       </div>
     )
   }

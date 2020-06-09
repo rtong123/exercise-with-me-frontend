1eba3168d71f4cac739b31fda28c9be372c5fe83
Rebecca Tong <rtong@buffalo.edu>
2020-06-09 11:52:52 -0400

    showing all exercises

--- exercise-with-me-frontend/src/components/ExerciseList.js
+++ exercise-with-me-frontend/src/components/ExerciseList.js
@@ -6,13 +6,18 @@ const Exercises = (props) => {
 
   return(
     <div>
+
       {props.exercises && props.exercises.map(exercise =>
         <li key={exercise.id}>
-          {exercise.name}
-          {exercise.equipment}
-          {exercise.sets}
-          {exercise.reps}
-         </li>)}
+
+          <h1>Exercises for Routine {exercise.routine_id}</h1>
+          <b>Exercise Name:</b>{exercise.name} <br></br>
+          <b>Exercise Equipment:</b>{exercise.equipment} <br></br>
+          <b>Exercise Sets:</b>{exercise.sets} <br></br>
+          <b>Exercise Reps:</b>{exercise.reps} <br></br>
+          <br></br>
+         </li>
+       )}
     </div>
   )
 }

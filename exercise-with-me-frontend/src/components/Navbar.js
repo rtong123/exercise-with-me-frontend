4720d923f3b63a4feab48447b8b552f6ebd5331e
Rebecca Tong <rtong@buffalo.edu>
2020-06-10 11:20:20 -0400

    adding nav bar

--- exercise-with-me-frontend/src/components/Navbar.js
+++ exercise-with-me-frontend/src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component{
 
 render(){
   return(
     <div>
-    hi
+      <ul id="nav">
+               <li><a href="#">Routines</a></li>
+               <li><Link to={'/routines/new'}>New Routine</Link></li>
+               <li><a href="#">FAQ</a></li>
+               <li><a href="#">Contact</a></li>
+      </ul>
     </div>
   )
 }

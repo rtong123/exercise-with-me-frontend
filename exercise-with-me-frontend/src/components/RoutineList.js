1aaad5c8cc3e1386474464db8f5e30a187617ab9
Rebecca Tong <rtong@buffalo.edu>
2020-06-23 18:19:46 -0400

    able to vote

--- exercise-with-me-frontend/src/components/RoutineList.js
+++ exercise-with-me-frontend/src/components/RoutineList.js
@@ -7,56 +7,52 @@ import styled, { css } from 'styled-components'
 
 class RoutineList extends React.Component{
 
- //  let button = styled.button`
- //   background: transparent;
- //   border-radius: 4px;
- //   border: 2px solid palevioletred;
- //   color: palevioletred;
- //   margin: 0 1em;
- //   padding: 0.25em 1em;
- // `;
 
- state = {
-   vote: 0
 
- }
+ constructor(props){
+   super(props);
+   this.state = { routines: props.routines }
+  }
 
-handleVote = (event,routineId) => {
-// if routine id matches the button
-console.log(event.target.value)
-
-  if (event.target.value == routineId){
+  componentWillReceiveProps(props) {
     this.setState({
-      vote: this.state.vote += 1
+      routines: props.routines
     })
   }
-  else{
-    console.log('nu')
-  }
+
+
+ handleVote = (routineId) => {
+   // if routine id matches the button
+  const updatedRoutine = this.state.routines.map(routine => {
+    if (routine.id == routineId) {
+      if (routine.votes) {
+          routine.votes += 1
+      } else {
+        routine.votes = 1
+      }
+    }
+    return routine;
+  })
+  this.setState({
+    routines: updatedRoutine
+  })
 }
 
-
-
-// this.setState({
-//   vote: this.state.vote + 1
-// })
-
-render(){
-  console.log(this.state.vote)
-return (
-  <div>
-    <h1>Exercise Routines</h1>
-
-    {this.props.routines.map(routine =>
-      <div key={routine.id}>
-
-        <Link to={`/routines/${routine.id}`}>{routine.title}</Link>
-          <button type="button" onClick={ (e) => this.handleVote(e,routine.id) } value={routine.id}> Vote</button>  {this.state.vote}
-     </div>)}
-
-
-  </div>
-  )
+  render(){
+  console.log(this.state.routines)
+    return (
+      <div>
+        <h1>Exercise Routines</h1>
+        {this.state.routines.map(routine =>
+          <div key={routine.id}>
+            <Link to={`/routines/${routine.id}`}>{routine.title}</Link>
+              <button type="button" onClick={() => this.handleVote(routine.id) } value={routine.id}>
+                Vote
+              </button>
+              {routine.votes}
+         </div>)}
+      </div>
+    )
   }
 }
 
